8f0af6de718a36a58d7e5501a6ea96750a8463a7
Ionut Balosin <ionutbalosin@yahoo.com>
2023-08-04 17:57:10 +0200

    Add DijkstraShortestPathBenchmark

--- benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/dijkstrashortestpath/DijkstraShortestPathBenchmark.java
+++ benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/dijkstrashortestpath/DijkstraShortestPathBenchmark.java
@@ -57,7 +57,7 @@ import org.openjdk.jmh.annotations.Warmup;
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1)
+@Fork(value = 5)
 @State(Scope.Benchmark)
 public class DijkstraShortestPathBenchmark {
 
