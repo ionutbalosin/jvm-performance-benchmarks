7e5c3e6b7d7ff326a83b34461c599e6d9046c141
Ionut Balosin <ionutbalosin@yahoo.com>
2023-08-16 13:28:53 +0200

    Add AES/DES/RSA Benchmarks

--- benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/crypto/AesCbcCryptoBenchmark.java
+++ benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/crypto/AesCbcCryptoBenchmark.java
@@ -56,9 +56,9 @@ import org.openjdk.jmh.annotations.Warmup;
  */
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Warmup(iterations = 3, time = 3, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = 3, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1)
+@Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 5)
 @State(Scope.Benchmark)
 public class AesCbcCryptoBenchmark {
 
