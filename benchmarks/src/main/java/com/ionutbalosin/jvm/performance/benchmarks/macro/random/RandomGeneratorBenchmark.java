807be769f490c321db0b281fb9f1156ac3f67650
Ionut Balosin <ionutbalosin@yahoo.com>
2023-08-22 14:47:12 +0200

    Add RandomBenchmark(s)

--- benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/random/RandomGeneratorBenchmark.java
+++ benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/random/RandomGeneratorBenchmark.java
@@ -54,7 +54,7 @@ import org.openjdk.jmh.annotations.Warmup;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1)
+@Fork(value = 5)
 @State(Scope.Benchmark)
 public class RandomGeneratorBenchmark {
 
