e69afe8f34929703bfc515305373cbf163c4c8f5
Florin Blanaru <florin.blanaru96@gmail.com>
2023-08-27 15:15:14 +0300

    Add Signature benchmarks (#69)
    * Add signature benchmarks

---
+++ benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/signature/DsaEdSignatureBenchmark.java
@@ -0,0 +1,87 @@
+/*
+ * JVM Performance Benchmarks
+ *
+ * Copyright (C) 2019 - 2023 Ionut Balosin
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.ionutbalosin.jvm.performance.benchmarks.macro.signature;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.NamedParameterSpec;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+/*
+ * This benchmark measures the performance of EDDSA (Edwards curve DSA) using different message lengths,
+ * key lengths.
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 5)
+@State(Scope.Benchmark)
+public class DsaEdSignatureBenchmark {
+
+  private Signature signer;
+
+  @Param({"64", "512", "2048", "16384"})
+  private int messageLength;
+
+  @Param({"Ed25519", "Ed448"})
+  private String algorithm;
+
+  private byte[] message;
+
+  @Setup
+  public void setup() throws Exception {
+    message = new byte[messageLength];
+    Random random = new Random(16384);
+    random.nextBytes(message);
+
+    KeyPairGenerator kpg = KeyPairGenerator.getInstance(algorithm, "SunEC");
+    NamedParameterSpec spec = new NamedParameterSpec(algorithm);
+    kpg.initialize(spec);
+    KeyPair kp = kpg.generateKeyPair();
+
+    signer = Signature.getInstance(algorithm, "SunEC");
+    signer.initSign(kp.getPrivate());
+  }
+
+  @Benchmark
+  public byte[] sign() throws SignatureException {
+    signer.update(message);
+    return signer.sign();
+  }
+}
