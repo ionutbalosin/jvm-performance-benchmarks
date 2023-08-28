b02049c60930b421a5df18ee5d72bf38d1c1b566
Ionut Balosin <ionutbalosin@yahoo.com>
2023-08-28 20:07:05 +0200

    Add Storage Benchmarks

---
+++ benchmarks/src/main/java/com/ionutbalosin/jvm/performance/benchmarks/macro/storage/util/FileUtil.java
@@ -0,0 +1,11 @@
+package com.ionutbalosin.jvm.performance.benchmarks.macro.storage.util;
+
+/*
+ * (c) 2023 Ionut Balosin
+ * Website: www.ionutbalosin.com
+ * Twitter: @ionutbalosin
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+public class FileUtil {
+}
