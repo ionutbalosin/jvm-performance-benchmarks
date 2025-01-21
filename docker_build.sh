cd3039a79489efb60211f18465b3ecd4c59d2d9d
Arkadiusz Czajkowski <arkadiusz.czajkowski@chainguard.dev>
2025-01-21 11:43:58 +0100

    Use docker build args for build

--- docker_build.sh
+++ docker_build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 IMAGE_NAME=$1
-JDK_VERSION=$2
+BUILD_PROFILE=$2
 JDK_IMAGE=$3
 JRE_IMAGE=$4
-BUILD_USER=$5
 
 if [ -z "$IMAGE_NAME" ]; then
   read -p "Enter the benchmark image name: " IMAGE_NAME
 fi
 
-if [ -z "$JDK_VERSION" ]; then
-  read -p "Enter the JDK version: " JDK_VERSION
+if [ -z "$BUILD_PROFILE" ]; then
+  read -p "Enter the mvn build profile: " BUILD_PROFILE
 fi
 
 if [ -z "$JDK_IMAGE" ]; then
@@ -22,22 +21,12 @@ if [ -z "$JRE_IMAGE" ]; then
   read -p "Enter the JRE image: " JRE_IMAGE
 fi
 
-if [ -z "$BUILD_USER" ]; then
-  read -p "Enter the username used for the build process [root]: " BUILD_USER
-fi
-
-BUILD_USER=${BUILD_USER:-root}
-
-export JDK_VERSION
-export JDK_IMAGE
-export JRE_IMAGE
-export BUILD_USER
-
-cat docker/Dockerfile.template | envsubst > docker/Dockerfile
-
 # Clean any local build artifacts to not copy them into the build image
 ./mvnw clean
 # Create the image
-docker build -t $IMAGE_NAME -f docker/Dockerfile .
-
-rm docker/Dockerfile
+docker build \
+  -f docker/Dockerfile . \
+  -t $IMAGE_NAME \
+  --build-arg BUILD_PROFILE=${BUILD_PROFILE} \
+  --build-arg JDK_IMAGE=${JDK_IMAGE} \
+  --build-arg JRE_IMAGE=${JRE_IMAGE}
